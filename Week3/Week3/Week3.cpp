8e052ea8933e736758cdc3dc984f41eb3a95d280
Kim GaEul <fallben@naver.com>
2023-09-24 00:32:23 +0900

    마지막 수정

--- Week3/Week3/Week3.cpp
+++ Week3/Week3/Week3.cpp
@@ -1,6 +1,5 @@
 ﻿#include <stdio.h>
 #include <conio.h>
-#include <iostream>
 #include <windows.h> // 불안정하지만, 제일 쓰기 편한 Sleep을 사용하기 위해
 
 int main()
@@ -16,62 +15,47 @@ int main()
                 bein = key; // 입력한 키에 좌측 화살표의 아스키코드값을 넣어준다
                 noin = 0; // 이전 입력이 무엇이었던, 해당 커맨드 이후 a를 눌러서 기술 발동은 불가능
                 printf("←");
-            }
-            else if (key == 77) {
+            } else if (key == 77) {
                 if (bein == 75) {
                     noin = 1; // 아따따뚜겐 기술 발현이 가능해진 상태
-                }
-                else if (bein == 80) {
+                } else if (bein == 80) {
                     noin = 2; // 아도겐 기술 발동이 가능
-                }
-                else {
+                } else {
                     bein = key; // 기술 발동이 불가능하다면, 입력한 키 초기화
                     noin = 0; // 또한 기술 발현이 불가능한 상태로 변경
                 }
                 printf("→");
-            }
-            else if (key == 72) {
+            } else if (key == 72) {
                 if (bein == 77) {
                     noin = 3; // 오류겐 기술 발동이 가능
-                }
-                else {
+                } else {
                     bein = key; // 기술 발동이 불가능하다면, 입력한 키 초기화
                     noin = 0; // 또한 기술 발현이 불가능한 상태로 변경
-                }
-                printf("↑");
-            }
-            else if (key == 80) {
+                } printf("↑");
+            } else if (key == 80) {
                 bein = key; // 발동 가능한 기술이 없으므로 입력한 키만 초기화
                 noin = 0; // 또한 기술 발현이 불가능한 상태로 변경
                 printf("↓");
-            }
-            else if (key == 97) {
+            } else if (key == 97) {
                 if (noin == 1) {
                     printf("a \t [아따따뚜겐!] <-> <-> <-> \n"); // 첫 번째 기술 발동 
-                }
-                else if (noin == 2) {
+                } else if (noin == 2) {
                     printf("a \t [아도겐!] =o \n"); // 두 번째 기술 발동 
-                }
-                else if (noin == 3) {
+                } else if (noin == 3) {
                     printf("a \t [오류겐!] _9↑↑ \n"); // 세 번째 기술 발동
-                }
-                else if (bein == 80) {
+                } else if (bein == 80) {
                     printf("a \t [짠손!] _o \n"); // 평타 기술 하나쯤은 있어야죠
-                }
-                else {
+                } else {
                     printf("a \t[평타!] ==@ \n"); // 위의 상황 모두와 안맞을 경우 기본 평타가 나감
-                }
+                } 
                 noin = 0;
                 bein = 0; // 이후 사용된 모든 전 입력 키들은 초기화
-            }
-            else {
-                if (key == -32) {
+            } else {
+                if (key == -32) { // 화살표 키를 누른 상황에서 왠지 뜨는 아스키코드 배제
                     continue;
-                }
-                else if (key == 27) {
+                } else if (key == 27) {
                     break; // ESC키 입력시 종료됨
-                }
-                else {
+                } else {
                     putchar(key);
                     noin = 0;
                     bein = 0;
